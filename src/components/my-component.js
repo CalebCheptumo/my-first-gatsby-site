c847ec5a1a0a42db20b09b9ca6f0212ed5c07481
calebcheptumo <ckibusia@gmail.com>
2023-12-16 20:49:19 +0300

    added basic styling

---
+++ src/components/my-component.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { link } from "../components/my-component.module.css";
+
+const MyComponent = () => {
+  return <h1 className={title}>Super title page</h1>;
+};
+
+export default MyComponent;

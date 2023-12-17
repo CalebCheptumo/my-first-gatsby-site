9672c700efd580ea872f7e3947ac32937b621297
calebcheptumo <ckibusia@gmail.com>
2023-12-17 14:52:13 +0300

    update blog page link

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,22 +1,21 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import { Link, graphql } from "gatsby";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
 const BlogPage = ({ data }) => {
   return (
-    <Layout pageTitle="My Blog Post">
-          {
-              
-                  data.allMdx.nodes.map((node) => (
-                      <article key={node.id}>
-                          <h2>{node.frontmatter.title}</h2>
-                          <p>Posted: {node.frontmatter.date}</p>
-                          <p>{node.excerpt}</p>
-                      </article>
-                  ))
-              }
-          
+    <Layout pageTitle="My Blog Post"> 
+      {data.allMdx.nodes.map(node => (
+        <article key={node.id}>
+          <h2>
+            <Link to={`/blog/${node.frontmatter.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+          </h2>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
@@ -27,9 +26,9 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
-        excerpt
       }
     }
   }

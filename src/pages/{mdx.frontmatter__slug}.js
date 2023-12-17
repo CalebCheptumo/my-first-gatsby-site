312f1f47f9e9920ba99ba0d4fc2eba8ed1289de5
calebcheptumo <ckibusia@gmail.com>
2023-12-17 14:05:13 +0300

    create blog post page tempate

---
+++ src/pages/{mdx.frontmatter__slug}.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+
+const BlogPost = () => {
+  return (
+    <Layout pageTitle="My Blogs">
+      <p>my blog articles will be here</p>
+    </Layout>
+  );
+};
+
+export const Head = () => <Seo title="Blog Post" />;
+
+export default BlogPost;
